5ab61583cb660e074a15ba308ab3b00ee5343a93
Lesedi Seema <156032629+Hv3y@users.noreply.github.com>
2024-05-16 16:34:31 +0200

    Add files via upload

---
+++ 2nd program/2nd program/2nd pro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int myNum = 5;
+	float myfloatNum = 8.97;
+	char myLetter = 'P';
+	bool mybolean = true;
+	string mystring = "HEllo";
+
+	cout << "int:" << myNum << "\n";
+	cout << "float:" << myfloatNum << "\n";
+	cout << "char:" << myLetter << "\n";
+	cout << "bool:" << mybolean << "\n";
+	cout << "string" << mystring << "\n";
+	return 0;
+}
